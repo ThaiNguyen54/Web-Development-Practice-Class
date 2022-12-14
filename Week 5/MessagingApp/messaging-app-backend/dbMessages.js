4e21aadf0858f8c04a7cc0e03484cc5d1c044e6d
ThaiNguyen54 <59272463+ThaiNguyen54@users.noreply.github.com>
2022-12-14 22:33:35 +0700

    updated messaging app backend

---
+++ Week 5/MessagingApp/messaging-app-backend/dbMessages.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const messagingSchema = mongoose.Schema({
+    message: String,
+    name: String,
+    timestamp: String,
+    received: Boolean
+})
+
+module.exports = mongoose.model('messagingmessages', messagingSchema)
